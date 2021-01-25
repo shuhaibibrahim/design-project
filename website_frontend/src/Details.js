d9f55ff36a22a651c94bcf6b7d8601d23c24c2eb
athulatk <insinexatk10@cet.ac.in>
2021-01-25 10:49:06 +0530

    website

--- website_frontend/src/Details.js
+++ website_frontend/src/Details.js
@@ -1,45 +1,54 @@
-import React,{useState} from 'react'
+import React,{useState,useEffect} from 'react'
 import Navbar1 from './Navbar1'
 import {Table} from 'react-bootstrap'
+import axios from 'axios'
 function Details() {
 
     const[date,setdate]=useState('');
+    const[data,setData]=useState([]);
+
+    useEffect(() => {
+      axios.get('https://design-server.herokuapp.com/vehicle')
+      .then(res=>{
+        console.log(res.data)
+        setData(res.data)
+      })
+    }, [])
 
     return (
         <div className="App">
             <Navbar1/>
-            <div>
+            <div style={{position:'absolute',right:'6%',marginTop:'2%',marginBottom:'2%'}}>
             <input type="date" value={date} onChange={(e)=>setdate(e.target.value)}/>
             </div>
+            <div style={{width:'80%',display:'block',margin:'auto',borderRadius:'5px'}}>
             <Table striped bordered hover style={{margin:'4em',backgroundColor:'white',opacity:'0.88'}}>
             <thead>
             <tr>
-                <th>#</th>
-                <th>First Name</th>
-                <th>Last Name</th>
-                <th>Username</th>
+                <th>Date</th>
+                <th>Time</th>
+                <th>License Plate Number</th>
             </tr>
             </thead>
   <tbody>
-    <tr>
-      <td>1</td>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
+    {
+      data.map(entry=>{
+        return(
+          <tr>
+      <td>{entry.date.slice(0,10)}</td>
+      <td>{entry.time}</td>
+      <td>{entry.pnumber}</td>
     </tr>
-    <tr>
-      <td>2</td>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <td>3</td>
-      <td colSpan="2">Larry the Bird</td>
-      <td>@twitter</td>
-    </tr>
+        )
+        
+      })
+    }
+
+
+   
   </tbody>
 </Table>
+</div>
         </div>
     )
 }
